3071e6eaa96aa4b5f43770f134f96b9bddccdb23
Sedratul Montahar Hossain <125445497+Infi421@users.noreply.github.com>
2024-02-23 00:41:33 +0600

    Add files via upload

---
+++ install_all.sh
@@ -0,0 +1,32 @@
+cd
+sudo apt install -y curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt -y install brave-browser
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+sudo apt install -y python3.8
+sudo apt install -y git
+mkdir -p ~/catkin_ws/src/
+cd ~/catkin_ws/src/
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+cd ~/catkin_ws && catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+cd
+print("DONE! Ready to go...");
+
+
+
+
+
+
+
+
+
+
